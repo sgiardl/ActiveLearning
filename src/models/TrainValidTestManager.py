07458e40c07d8f2988cd7babfcfab0c2c5d6d2a3
gias2402 <gias2402@usherbrooke.ca>
2021-04-01 20:17:24 -0400

    Changed multiple training parameters to __init__

--- src/models/TrainValidTestManager.py
+++ src/models/TrainValidTestManager.py
@@ -2,7 +2,7 @@
 This file stores all functions related to model training.
 """
 import os
-
+import numpy as np
 import torch
 import torch.backends.cudnn as cudnn
 from torch.utils.data import Subset
@@ -18,13 +18,41 @@ class TrainValidTestManager():
     def __init__(self, dataset_manager: DatasetManager,
                  file_name: str,
                  model_name: str,
+                 learning_rate: float,
                  pretrained: bool = False):
         self.data_loader_train = dataset_manager.data_loader_train
         self.file_name = file_name
-        self.model_name = model_name
-        self.pretrained = pretrained
 
-    def train_model(self, epochs: int, **kwargs) -> None:
+        # Define device as the GPU if available, else use the CPU
+        self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+        # Number of classes in the data_loader dataset
+        if type(self.data_loader_train.dataset) == Subset:
+            num_classes = len(self.data_loader_train.dataset.dataset.class_to_idx)
+        else:
+            num_classes = len(self.data_loader_train.dataset.class_to_idx)
+
+        # Get model and set last fully-connected layer with the right
+        # number of classes
+        self.model = load_zoo_models(model_name, num_classes, pretrained=pretrained)
+
+        # Define loss function
+        self.criterion = torch.nn.CrossEntropyLoss()
+
+        # Find which parameters to train (those with .requires_grad = True)
+        params = [p for p in self.model.parameters() if p.requires_grad]
+
+        # Define Adam optimizer
+        self.optimizer = torch.optim.Adam(params, lr=learning_rate)
+
+        # Send the model to the device
+        self.model.to(self.device)
+
+        # Get length of data_loader once
+        self.len_data_loader = len(self.data_loader_train)
+
+
+    def train_model(self, epochs: int) -> None:
         """
         Trains the model and saves the trained model.
 
@@ -39,73 +67,46 @@ class TrainValidTestManager():
         # to find the best algorithm to use for the hardware used
         cudnn.benchmark = True
 
-        # Number of classes in the data_loader dataset
-        if type(self.data_loader_train.dataset) == Subset:
-            num_classes = len(self.data_loader_train.dataset.dataset.class_to_idx)
-        else:
-            num_classes = len(self.data_loader_train.dataset.class_to_idx)
-
-        # Get model and set last fully-connected layer with the right
-        # number of classes
-        model = load_zoo_models(self.model_name, num_classes, pretrained=self.pretrained)
-
-        # Define loss function
-        criterion = torch.nn.CrossEntropyLoss()
-
-        # Find which parameters to train (those with .requires_grad = True)
-        params = [p for p in model.parameters() if p.requires_grad]
-
-        # Define Adam optimizer
-        optimizer = torch.optim.Adam(params, lr=kwargs.get('learning_rate', LEARNING_RATE))
-
-        # Define device as the GPU if available, else use the CPU
-        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-        # Send the model to the device
-        model.to(device)
-
-        # Get length of data_loader once
-        len_data_loader = len(self.data_loader_train)
-
-        # Initialize loss = 0 and loss_list as a blank list
-        loss_list = []
-
         # Specify that the model will be trained
-        model.train()
+        self.model.train()
 
         # Declare tqdm progress bar
-        pbar = tqdm(total=len_data_loader, leave=False,
+        pbar = tqdm(total=self.len_data_loader, leave=False,
                     desc='Epoch 0', postfix='Loss: 0')
+
+        loss_list = []
+        accuracy_list = []
+        fig, (ax1, ax2) = plt.subplots(1, 2)
 
         # Main training loop, loop through each epoch
         for epoch in range(epochs):
+            loss_list_epoch = []
+            accuracy_list_epoch = []
+
             # Loop through each mini-batch from the data loader
             for i, (images, labels) in enumerate(self.data_loader_train):
                 # Send images and labels to the device
-                images, labels = images.to(device), labels.to(device)
+                images, labels = images.to(self.device), labels.to(self.device)
 
                 # Reset all gradients to zero
-                optimizer.zero_grad()
+                self.optimizer.zero_grad()
 
                 # Perform a forward pass
-                outputs = model.forward(images)
-
-                # Get the maximum prediction & class associated with
-                # the maximum prediction
-                # max_pred, max_class = preds.topk(1, dim=1)
+                outputs = self.model.forward(images)
 
                 # Calculate the loss, comparing outputs with the
                 # ground truth labels
-                loss = criterion(outputs, labels)
+                loss = self.criterion(outputs, labels)
 
                 # Appending the current loss to the loss list
-                loss_list.append(loss)
+                loss_list_epoch.append(loss.item())
+                accuracy_list_epoch.append(self.get_accuracy(outputs, labels))
 
                 # Perform a backward pass (calculate gradient)
                 loss.backward()
 
                 # Perform a parameter update based on the git current gradient
-                optimizer.step()
+                self.optimizer.step()
 
                 # Update progress bar
                 pbar.set_description_str(f'Epoch {epoch}')
@@ -115,13 +116,27 @@ class TrainValidTestManager():
             # Reset progress bar after epoch completion
             pbar.reset()
 
+            loss_list.append(np.mean(loss_list_epoch))
+            accuracy_list.append(np.mean(accuracy_list_epoch))
+
         # Display and format chart of loss per iteration
-        plt.plot(loss_list)
-        plt.title('Loss per iteration')
-        plt.xlabel('Iteration')
-        plt.ylabel('Loss')
-        plt.show()
+        ax1.plot(loss_list, marker='.')
+        ax1.set_title('Mean loss per epoch')
+        ax1.set(xlabel='Epoch', ylabel='Mean loss')
+
+        ax2.plot(accuracy_list, marker='.')
+        ax2.set_title('Mean accuracy per epoch')
+        ax2.set(xlabel='Epoch', ylabel='Mean accuracy')
+
+        fig.tight_layout()
+        fig.show()
 
         # If file_name is specified, save the trained model
         if self.file_name is not None:
-            torch.save(model.state_dict(), f'{os.getcwd()}/models/{self.file_name}')
+            torch.save(self.model.state_dict(), f'{os.getcwd()}/models/{self.file_name}')
+
+    # def validate_model(self):
+    #
+
+    def get_accuracy(self, outputs, labels):
+        return (outputs.argmax(dim=1) == labels).sum().item() / labels.size(0)
