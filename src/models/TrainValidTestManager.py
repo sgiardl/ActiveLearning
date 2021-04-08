c4e8e2e5ebfe6d6c3af8f6087e68a83676f0cd08
gias2402 <gias2402@usherbrooke.ca>
2021-04-08 11:46:29 -0400

    Moved load_zoo_models to the TrainValidTest manager class

--- src/models/TrainValidTestManager.py
+++ src/models/TrainValidTestManager.py
@@ -4,13 +4,16 @@ This file stores all functions related to model training.
 import os
 import numpy as np
 import torch
-from torch.utils.data import Subset, Dataset, DataLoader
+from torch.utils.data import Subset, DataLoader
 from torch import tensor
 from torch.nn.functional import softmax
 from src.data.DataLoaderManager import DataLoaderManager
-from .model import load_zoo_models
 from tqdm import tqdm
-from typing import Sequence, Union
+import torchvision.models as models
+import torch.nn as nn
+from .constants import *
+
+MODELS = [RESNET34, SQUEEZE_NET_1_1]
 
 
 class TrainValidTestManager:
@@ -51,7 +54,7 @@ class TrainValidTestManager:
             num_classes = len(self.data_loader_train.dataset.class_to_idx)
 
         # Get model and set last fully-connected layer with the right number of classes
-        self.model = load_zoo_models(model_name, num_classes, pretrained=pretrained)
+        self.model = self.load_zoo_models(model_name, num_classes, pretrained=pretrained)
 
         # Define loss function
         self.criterion = torch.nn.CrossEntropyLoss()
@@ -236,6 +239,43 @@ class TrainValidTestManager:
             outputs = self.model.forward(images)
 
         return softmax(outputs, dim=1)
+
+    @staticmethod
+    def load_zoo_models(name: str, num_classes: int, pretrained: bool = False) -> nn.Module:
+        """
+        Loads model from torchvision and changes last layer if pretrained = True
+
+        :param name: Name of the model must be in MODELS list
+        :param num_classes: Number of classes in the last fully connected layer (nn.Linear)
+        :param pretrained: bool indicating if we want the pretrained version of the model on ImageNet
+        :return: PyTorch Model
+
+        The finetuning procedure is inspired from :
+        https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tutorial.html
+        """
+        assert name in MODELS, f"The name provided must be in {MODELS}"
+
+        if name == RESNET34:
+            if pretrained:
+                # If pretrained, an error occur if num_classes != 1000,
+                # we have to initialize and THEN change the last layer
+                m = models.resnet34(pretrained)
+                m.fc = nn.Linear(512, num_classes)
+            else:
+                # If not pretrained, the last layer can be of any size, hence we can do both step
+                # in one and avoid initializing last layer twice
+                m = models.resnet34(pretrained, num_classes=num_classes)
+        else:
+            if pretrained:
+                # If pretrained, an error occur if num_classes != 1000,
+                # we have to initialize and THEN change the last layer
+                m = models.squeezenet1_1(pretrained)
+                m.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1, 1), stride=(1, 1))
+            else:
+                # If not pretrained, the last layer can be of any size, hence we can do both step
+                # in one and avoid initializing last layer twice
+                m = models.squeezenet1_1(pretrained, num_classes=num_classes)
+        return m
 
     @staticmethod
     def get_accuracy(outputs: torch.Tensor, labels: torch.Tensor) -> float:
