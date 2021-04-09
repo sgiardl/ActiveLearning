8fe064c937b7a319ac8dcaf194aac1b7f5efa65c
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-04-09 11:49:30 -0400

    New experimentation file

---
+++ Experiments/Generalization.py
@@ -0,0 +1,48 @@
+import sys
+import os
+import subprocess as sp
+import time
+
+from src.models.constants import SQUEEZE_NET_1_1, RESNET34
+from src.data.constants import CIFAR10, EMNIST
+
+FIXED_SQUEEZENET_PARAM = ['--model', SQUEEZE_NET_1_1, '--dataset', CIFAR10, '--n_start', '100',
+                          '--n_new', '10', '--epochs', '25', '--query_strategy', 'least_confident',
+                          '--batch_size', '50', '--lr', '0.0001', '--n_rounds', '20', 'patience', '4']
+
+FIXED_RESNET_PARAM = ['--model', RESNET34, '--dataset', EMNIST, '--n_start', '25',
+                      '--n_new', '10', '--epochs', '25', '--query_strategy', 'least_confident',
+                      '--batch_size', '50', '--lr', '0.0001', '--n_rounds', '10', 'patience', '4']
+
+FIXED_CMDS = ['python3', str(os.path.join('..', 'train.py'))]
+
+
+def generate_combinations(fixed_model_param):
+    return [FIXED_CMDS + fixed_model_param + ['--weight_decay', '0'],
+            FIXED_CMDS + fixed_model_param + ['--data_aug'],
+            FIXED_CMDS + fixed_model_param + ['--data_aug', '--weight_decay', '0.001'],
+            FIXED_CMDS + fixed_model_param + ['--data_aug', '--weight_decay', '0.005']]
+
+
+if __name__ == '__main__':
+
+    # Resnet commands list
+    resnet_cmds = generate_combinations(FIXED_RESNET_PARAM)
+
+    # SqueezeNet commands list
+    squeezenet_cmds = generate_combinations(FIXED_RESNET_PARAM)
+
+    # List of all cmds to run
+    cmds = squeezenet_cmds + resnet_cmds
+
+    # We run experiments
+    start = time.time()
+    for cmd in cmds:
+        print(cmd)
+        p = sp.Popen(cmd)
+        p.wait()
+
+    print("Time Taken (minutes): ", round((time.time() - start) / 60, 2))
+
+
+
