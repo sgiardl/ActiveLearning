6afd64f963e64c43a6acc0fd41abb967b19fc040
riaa3102 <Abir.Riahi@USherbrooke.ca>
2021-04-09 02:25:39 -0400

    Add parameters

--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 import argparse
-import sys
 from src.models.ActiveLearning import ActiveLearner
 
 
@@ -11,6 +10,10 @@ def argument_parser():
     parser.add_argument('--n_new', type=int, default=100)
     parser.add_argument('--epochs', type=int, default=50)
     parser.add_argument('--query_strategy', type=str, default='least_confident')
+    parser.add_argument('--experiment_name', type=str, default='test')
+    parser.add_argument('--batch_size', type=int, default=50)
+    parser.add_argument('--lr', type=float, default=0.0001)
+    parser.add_argument('--weight_decay', type=float, default=0)
     return parser.parse_args()
 
 
@@ -24,10 +27,14 @@ if __name__ == "__main__":
     n_new = args.n_new
     epochs = args.epochs
     query_strategy = args.query_strategy
+    experiment_name = args.experiment_name
+    batch_size = args.batch_size
+    lr = args.lr
+    weight_decay = args.weight_decay
 
     active_learner = ActiveLearner(model, dataset, n_start=n_start, n_new=n_new, epochs=epochs,
-                                   query_strategy=query_strategy, experiment_name="test",
-                                   batch_size=50, lr=0.0001, weight_decay=0,
+                                   query_strategy=query_strategy, experiment_name=query_strategy,
+                                   batch_size=batch_size, lr=lr, weight_decay=weight_decay,
                                    pretrained=False, data_aug=False)
 
     active_learner(n_rounds=3)
