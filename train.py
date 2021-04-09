0188d77c0691f5f95ae010b079ddee9e5346a917
riaa3102 <Abir.Riahi@USherbrooke.ca>
2021-04-09 02:34:53 -0400

    Add help

--- train.py
+++ train.py
@@ -5,21 +5,36 @@ from src.models.ActiveLearning import ActiveLearner
 def argument_parser():
     parser = argparse.ArgumentParser(usage='\n python3 train.py [model] [dataset] [hyper_parameters]')
     parser.add_argument('--model', type=str, default='SqueezeNet11',
-                        choices=['SqueezeNet11', 'ResNet34'])
+                        choices=['SqueezeNet11', 'ResNet34'],
+                        help='Name of the model to train '
+                             '("ResNet34" or "SqueezeNet11")')
     parser.add_argument('--dataset', type=str, default='CIFAR10',
-                        choices=['CIFAR10', 'EMNIST'])
-    parser.add_argument('--n_start', type=int, default=100)
-    parser.add_argument('--n_new', type=int, default=100)
-    parser.add_argument('--epochs', type=int, default=50)
+                        choices=['CIFAR10', 'EMNIST'],
+                        help='Name of the dataset to learn on '
+                             '("CIFAR10" or "EMNIST")')
+    parser.add_argument('--n_start', type=int, default=100,
+                        help='The number of items that must be randomly '
+                             'labeled in each class by the Expert')
+    parser.add_argument('--n_new', type=int, default=100,
+                        help='The number of new items that must be labeled '
+                             'within each active learning loop')
+    parser.add_argument('--epochs', type=int, default=50,
+                        help='Number of training epochs in each active learning '
+                             'loop')
     parser.add_argument('--query_strategy', type=str, default='least_confident',
                         choices=['random_sampling', 'least_confident', 'margin_sampling',
-                                 'entropy_sampling'])
+                                 'entropy_sampling'],
+                        help='Query strategy of the expert')
     parser.add_argument('--experiment_name', type=str, default='least_confident',
                         choices=['random_sampling', 'least_confident', 'margin_sampling',
-                                 'entropy_sampling'])
-    parser.add_argument('--batch_size', type=int, default=50)
-    parser.add_argument('--lr', type=float, default=0.0001)
-    parser.add_argument('--weight_decay', type=float, default=0)
+                                 'entropy_sampling'],
+                        help='Name of the active learning experiment')
+    parser.add_argument('--batch_size', type=int, default=50,
+                        help='Batch size of dataloaders storing train, valid and test set')
+    parser.add_argument('--lr', type=float, default=0.0001,
+                        help='Learning rate of the model during training')
+    parser.add_argument('--weight_decay', type=float, default=0,
+                        help='The regularization term')
     return parser.parse_args()
 
 
