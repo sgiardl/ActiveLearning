af60473e86ce936e994968c6fdcc40f70e0100b1
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-04-11 11:45:37 -0400

    PEP8 error removed

--- Experiments/Pretraining.py
+++ Experiments/Pretraining.py
@@ -6,12 +6,12 @@ from src.data.constants import CIFAR10, EMNIST
 from Helpers import send_experiment_cmds
 EXPERIMENT_NAME = 'pretraining'
 
-FIXED_SQUEEZENET_CMDS = ['-en', EXPERIMENT_NAME,'--model', SQUEEZE_NET_1_1, '--dataset', CIFAR10, '--n_start', '100',
+FIXED_SQUEEZENET_CMDS = ['-en', EXPERIMENT_NAME, '--model', SQUEEZE_NET_1_1, '--dataset', CIFAR10, '--n_start', '100',
                          '--n_new', '100', '--epochs', '10', '--query_strategy', 'least_confident',
                          '--batch_size', '50', '--learning_rate', '0.0001', '--n_rounds', '20', '--patience', '3',
                          '--data_aug', '--weight_decay', '0.005']
 
-FIXED_RESNET_CMDS = ['-en', EXPERIMENT_NAME,'--model', RESNET18, '--dataset', EMNIST, '--n_start', '50',
+FIXED_RESNET_CMDS = ['-en', EXPERIMENT_NAME, '--model', RESNET18, '--dataset', EMNIST, '--n_start', '50',
                      '--n_new', '1000', '--epochs', '10', '--query_strategy', 'least_confident',
                      '--batch_size', '50', '--learning_rate', '0.0001', '--n_rounds', '10', '--patience', '3',
                      '--data_aug', '--weight_decay', '0.005']
