e8695f462b459fe91ec5b4e33689691c05b17f97
rayn2402 <nicolas.raymond2@usherbrooke.ca>
2021-04-12 00:19:43 -0400

    New file with argparse to get results plots

---
+++ extract_results_plots.py
@@ -0,0 +1,51 @@
+from src.visualization.VisualizationManager import VisualizationManager
+from src.models.constants import SQUEEZE_NET_1_1, RESNET18
+import argparse
+
+
+def argument_parser():
+    """
+    This function defines a parser to enable user to retrieve results plot
+    """
+    # Create a parser
+    parser = argparse.ArgumentParser(usage='\n python3 train.py [model] [dataset] [hyper_parameters]',
+                                     description="This program enables user to get validation accuracy plots"
+                                                 " from records.json files.")
+
+    parser.add_argument('-m', '--model', type=str, default=SQUEEZE_NET_1_1,
+                        choices=[SQUEEZE_NET_1_1, RESNET18],
+                        help=f"Name of the model to train ({SQUEEZE_NET_1_1} or {RESNET18})")
+
+    parser.add_argument('-fp', '--folder_prefix', type=str, default='query_strategy',
+                        help=f"Start of the folders name from which to extract results")
+
+    parser.add_argument('-c', '--curve_label', type=str, default='query_strategy',
+                        help=f"Labels to use in order to compare validation accuracy curve")
+
+    parser.add_argument('-s', '--save_path', type=str, default='accuracy_curves',
+                        help=f"Name of the file containing the resulting plot")
+
+    args = parser.parse_args()
+
+    # Print arguments
+    print("\nThe inputs are:")
+    for arg in vars(args):
+        print("{}: {}".format(arg, getattr(args, arg)))
+    print("\n")
+
+    return args
+
+
+if __name__ == '__main__':
+
+    # We extract arguments
+    args = argument_parser()
+    prefix = args.folder_prefix
+    model = args.model
+    curve_label = args.curve_label
+    save_path = args.save_path
+
+    # We produce the plot
+    vm = VisualizationManager()
+    vm.show_learning_curve(folder_prefix=prefix, model=model,
+                           save_path=save_path, curve_label=curve_label)
